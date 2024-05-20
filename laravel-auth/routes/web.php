40974b2f53ac0f54cdbfd5ecc8149242cb6e191f
devitoangelo <angelodevito4@gmail.com>
2024-05-20 19:06:02 +0200

    implement the index method and add style layout

--- laravel-auth/routes/web.php
+++ laravel-auth/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,7 +23,10 @@ Route::get('/', function () {
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
 
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('post', PostController::class);
 });
+
+
 
 
 
