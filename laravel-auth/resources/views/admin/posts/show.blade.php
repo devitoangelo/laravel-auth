1fac1a4f549f359a6514557abd5a70894d7fc8f5
devitoangelo <angelodevito4@gmail.com>
2024-05-20 19:13:21 +0200

    add method show

---
+++ laravel-auth/resources/views/admin/posts/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.admin')
+
+
+@section('content')
+    <header class="py-3 bg-dark text-white">
+        <div class="container">
+            <h1>{{ $post->title }}</h1>
+        </div>
+    </header>
+
+    <section class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <img width="100%" src="{{ $post->cover_image }}" alt="{{ $post->title }}">
+                </div>
+                <div class="col">
+                    <p> <strong>Title</strong> : {{ $post->title }}</p>
+                    <p><strong>Description</strong> : {{ $post->content }}</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+@endsection
